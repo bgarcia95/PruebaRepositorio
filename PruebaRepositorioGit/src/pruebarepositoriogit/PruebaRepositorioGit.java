26659b36a21d330f2af2d503f33b6998cce5ef88
bryanslur <elgordo_slur@hotmail.com>
2016-08-09 22:51:11 -0600

    Segundo cambio

--- PruebaRepositorioGit/src/pruebarepositoriogit/PruebaRepositorioGit.java
+++ PruebaRepositorioGit/src/pruebarepositoriogit/PruebaRepositorioGit.java
@@ -10,12 +10,41 @@ package pruebarepositoriogit;
  * @author elgor
  */
 public class PruebaRepositorioGit {
-
+    private String nombre;
+    private int valor;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the valor
+     */
+    public int getValor() {
+        return valor;
+    }
+
+    /**
+     * @param valor the valor to set
+     */
+    public void setValor(int valor) {
+        this.valor = valor;
     }
     
 }
